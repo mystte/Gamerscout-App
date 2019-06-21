37d25f26d4db3ffb3307bbb8c1472ceb44ce9ff3
Stéphane ROSE <srose@turbulent.ca>
2019-06-21 11:42:30 -0300

    removed unused error for now

--- src/react/components/containers/navbar/_ui/signuppopup/SignupPopup.js
+++ src/react/components/containers/navbar/_ui/signuppopup/SignupPopup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 // import PropTypes from 'prop-types';
 
 import Localization from '../../../../../config/localization/Localization';
@@ -11,7 +11,6 @@ import { POPUP_TYPE } from '../../../../../datamanager/models/PopupRecord';
 
 const SigninPopup = () => {
   const labels = Localization.Labels.signupPopup;
-  const error = useSelector(state => state.error);
   const dispatch = useDispatch();
 
   return (

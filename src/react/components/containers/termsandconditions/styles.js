23fc52860f7407b331722957089ae622febb9b82
Stéphane ROSE <srose@turbulent.ca>
2019-12-23 22:03:24 -0500

    terms and conditions

--- src/react/components/containers/termsandconditions/styles.js
+++ src/react/components/containers/termsandconditions/styles.js
@@ -1,5 +1,44 @@
-const styles = {
+import { colorNameToRgba, colorNameToHex } from '../../../utils/color';
 
+const styles = {
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 24,
+    color: colorNameToHex('white'),
+    alignSelf: 'flex-start',
+    marginLeft: '9vw',
+    marginTop: 30,
+  },
+  selected: {
+    color: colorNameToHex('white'),
+    borderBottom: `1px solid ${colorNameToHex('dodgerblue')}`,
+    cursor: 'pointer',
+  },
+  link: {
+    color: colorNameToHex('regentgray'),
+    textDecoration: 'none',
+    marginRight: 20,
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    textTransform: 'uppercase',
+    fontWeight: 600,
+    fontSize: 12,
+    color: colorNameToHex('regentgray'),
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  textBloc: {
+    width: 820,
+    textAlign: 'justify',
+    color: colorNameToRgba('white', 0.7),
+    fontSize: 16,
+    marginBottom: 150,
+  },
 };
 
 export default styles;

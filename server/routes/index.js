e6d2ba4098bb6e4816d67c6606d584b8a5c911b9
Stéphane ROSE <srose@turbulent.ca>
2020-01-25 00:24:35 -0500

    pki

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,23 @@
 const path = require('path');
 const router = require('express').Router();
 
-router
-  .get('/*', (req, res, next) => {
-    const routePath = path.join(__dirname + '..', '..', '..', 'dist/' + 'index.html');
-    res.sendFile(routePath);
-  })
+router.get(
+  '/.well-known/pki-validation/3F005EF50CBA24C5836A32ABC5CA9E59.txt',
+  (req, res, next) => {
+    res.send(
+      '036416679B0B4CD572D7BE08B6B81081061B1C9F880422269FF9127F759B2177 comodoca.com 5e290296010ca'
+    );
+  }
+);
+
+router.get('/*', (req, res, next) => {
+  const routePath = path.join(
+    __dirname + '..',
+    '..',
+    '..',
+    'dist/' + 'index.html'
+  );
+  res.sendFile(routePath);
+});
 
 module.exports = router;

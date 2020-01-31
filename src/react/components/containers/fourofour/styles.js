4cb19b342bd3b226606515619e1ff7d057f41e91
Stéphane ROSE <srose@turbulent.ca>
2020-01-31 00:03:04 -0500

    Privacy policy typo + 404

---
+++ src/react/components/containers/fourofour/styles.js
@@ -0,0 +1,27 @@
+const styles = {
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+
+  title: {
+    fontSize: 72,
+    fontWeight: 600,
+    marginTop: 30,
+    marginBottom: 10,
+    height: 55,
+  },
+
+  desc: {
+    fontSize: 16,
+    marginBottom: 30,
+  },
+
+  goHome: {
+    width: 150,
+    marginBottom: 100,
+  },
+};
+
+export default styles;

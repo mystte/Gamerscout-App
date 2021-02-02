4c0b0b5eaf61424fc69b80cc63ac7b5e85a644e1
Stéphane ROSE <srose@turbulent.ca>
2021-02-01 22:48:30 -0500

    added new csr

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,19 +2,10 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get(
-  '/.well-known/pki-validation/4692FC9AF913B6E25BAFFA9C5B33ADF8.txt',
+  '/.well-known/pki-validation/E01CA2715945306933C22EFE68435398.txt',
   (req, res, next) => {
     res.send(
-      '70CB24D622CCB84FAC8F6EEA6883DFB15A1092C48AEF1081BB039587C1E7B4A6 comodoca.com 5e2c4b0c2108e'
-    );
-  }
-);
-
-router.get(
-  '/.well-known/pki-validation/F4D80C696A4902B6E202A466C9E5BA18.txt',
-  (req, res, next) => {
-    res.send(
-      'AFFD57E888B584FC827F8356DC2156B77D1F6324139C9CF260144C663B8BE2F4 comodoca.com 5e3a032fed0c9'
+      '8E9841FF8BA6F3B0B6BE30AAA6504FA88C7B69BA5750C8BF73E95D002D8EB1D2 comodoca.com 6018c9137e7e8'
     );
   }
 );
